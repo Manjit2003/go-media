ed3b1a0c14748174ed68ac92697bed104983cf0b
Manjit Pardeshi <manjitpardeshi91991@gmail.com>
2022-08-05 16:14:06 +0000

    feat: ffmpeg working!

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Manjit2003/go-media
 
 go 1.17
+
+require github.com/u2takey/ffmpeg-go v0.4.1
+
+require (
+	github.com/aws/aws-sdk-go v1.38.20 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	if err := ffmpeg.Input("examples/example1.mp4").Output("examples/output.m3u8", ffmpeg.KwArgs{
+		"b:v":              "1M",
+		"g":                "60",
+		"format":           "hls",
+		"hls_time":         "2",
+		"hls_segment_size": "500000",
+		"hls_list_size":    "0",
+		"hls_flags":        "delete_segments",
+	}).ErrorToStdOut().Run(); err != nil {
+		panic(err)
+	}
+
 }
